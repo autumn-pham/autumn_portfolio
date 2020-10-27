f1e7e84beba19c87f3290001c5d0b9313ed4e594
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-10-27 08:59:06 -0700

    styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,13 +5,12 @@ class Main extends React.Component {
   render() {
     return (
       <div className="main col-9">
-        <hr/><h4 id="about">about autumn</h4><hr/>
+        <hr/><h3 id="about">about autumn</h3><hr/>
         <p>As a full-stack developer, I bring x, y, and z to my work.</p>
-        <hr/><h4 id="projects">projects</h4><hr/>
+        <hr/><h3 id="projects">projects</h3><hr/>
         <Project/>
-        <hr/><h4>experience</h4><hr/>
-        <h6>Skills</h6>
-        <span className="badge badge-pill badge-dark">html</span> <span className="badge badge-pill badge-dark">css</span> <span className="badge badge-pill badge-dark">responsive design</span> <span className="badge badge-pill badge-dark">object-oriented design</span> <span className="badge badge-pill badge-dark">javascript</span>  <span className="badge badge-pill badge-dark">react</span> <span className="badge badge-pill badge-dark">jquery</span> <span className="badge badge-pill badge-dark">node.js</span> <span className="badge badge-pill badge-dark">express</span> <span className="badge badge-pill badge-dark">php</span> <span className="badge badge-pill badge-dark">mongodb</span> <span className="badge badge-pill badge-dark">mongoose</span> <span className="badge badge-pill badge-dark">postgresql</span>
+        <hr/><h3 id="experience">experience</h3><hr/>
+
         <p>View my full resume <a href="#">here</a>.</p>
       </div>
     );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,14 +7,18 @@ class Sidebar extends React.Component {
       <div className="sidebar col-3">
           <img src={Autumn} alt="autumn photo" className="autumn-img"/>
           <h3 className="name">Autumn Pham</h3>
-          <span className="email"><i className="far fa-envelope"></i> ph.autumn@gmail.com</span><br/>
+          <span className="email"><i className="far fa-envelope"></i> ph.autumn@gmail.com</span><hr/>
           <a href="#about">About</a><br/>
-          <a href="#projects">Projects</a>
+          <a href="#projects">Projects</a><br/>
+          <a href="#experience">Experience</a><hr/>
           <div className="social-icons">
             <a href="https://www.linkedin.com/in/autumn-pham" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-2x"></i></a><br/>
             <a href="https://github.com/autumn-pham" target="_blank" rel="noreferrer"><i className="fab fa-github-square fa-2x"></i></a><br/>
             <a href="https://github.com/autumn-pham" target="_blank" rel="noreferrer"><i className="fas fa-file fa-2x"></i></a><br/>
-
+          </div><hr/>
+          <div className="skills">
+            <h6>Skills</h6>
+            <span className="badge badge-pill badge-dark">html</span> <span className="badge badge-pill badge-dark">css</span> <span className="badge badge-pill badge-dark">responsive design</span> <span className="badge badge-pill badge-dark">object-oriented design</span> <span className="badge badge-pill badge-dark">javascript</span>  <span className="badge badge-pill badge-dark">react</span> <span className="badge badge-pill badge-dark">jquery</span> <span className="badge badge-pill badge-dark">node.js</span> <span className="badge badge-pill badge-dark">express</span> <span className="badge badge-pill badge-dark">php</span> <span className="badge badge-pill badge-dark">mongodb</span> <span className="badge badge-pill badge-dark">mongoose</span> <span className="badge badge-pill badge-dark">postgresql</span>
           </div>
         </div>
     );
