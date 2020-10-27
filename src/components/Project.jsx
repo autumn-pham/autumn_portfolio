5499cbd8ee4fbed7045eaad7038cbdd7bcf5f948
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-10-26 21:14:55 -0700

    fixing cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,28 +11,27 @@ class Project extends React.Component {
         <div className="card">
           <div className="card-header">Fighting Fit<span className="link-btn"><a href="https://fightingfit.herokuapp.com" className="btn btn-outline-dark" role="button" target="_blank" rel="noreferrer"><i className="fas fa-external-link-alt"></i></a></span></div>
           <img src={FightingFit} className="card-img-top" alt="fighting fit site"/>
-          <div className="card-body">
-
+          <div className="card-img-overlay">
           </div>
         </div>
         <div className="card">
           <div className="card-header">Fighting Fit Meetup<span className="link-btn"><a href="#" className="btn btn-outline-dark" role="button" target="_blank" rel="noreferrer"><i className="fas fa-external-link-alt"></i></a></span></div>
           <img src={FightingFitMeetup} className="card-img-top" alt="fighting fit meetup site"/>
-          <div className="card-body">
+          <div className="card-img-overlay">
 
           </div>
         </div>
         <div className="card">
           <div className="card-header">Pinch of Salt<span className="link-btn"><a href="https://pinch-of-salt.herokuapp.com" className="btn btn-outline-dark" role="button" target="_blank" rel="noreferrer"><i className="fas fa-external-link-alt"></i></a></span></div>
           <img src={PinchofSalt} className="card-img-top" alt="pinch of salt site"/>
-          <div className="card-body">
+          <div className="card-img-overlay">
 
           </div>
         </div>
         <div className="card">
           <div className="card-header">A Novel Idea<span className="link-btn"><a href="https://infinite-stream-72673.herokuapp.com" className="btn btn-outline-dark" role="button" target="_blank" rel="noreferrer"><i className="fas fa-external-link-alt"></i></a></span></div>
           <img src={NovelIdea} className="card-img-top" alt="..."/>
-          <div className="card-body">
+          <div className="card-img-overlay">
 
           </div>
         </div>
