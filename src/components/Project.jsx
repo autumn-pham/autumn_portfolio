acf12e79ead079b9705b6baf5b33aae7dd8e42a4
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-10-26 18:51:02 -0700

    added projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import FightingFit from "../assets/fightingfit.png";
+import FightingFitMeetup from "../assets/fightingfitmeetup.png";
+import PinchofSalt from "../assets/pinchofsalt.png";
 
 class Project extends React.Component {
   render() {
@@ -6,30 +9,30 @@ class Project extends React.Component {
       <div className="project">
         <div className="card">
           <div className="card-header">Fighting Fit</div>
-          <img src="" className="card-img-top" alt="..."/>
+          <img src={FightingFit} className="card-img-top" alt="fighting fit site"/>
           <div className="card-body">
-            <a href="#" className="btn btn-outline-dark" role="button">View Project</a>
+            <a href="https://fightingfit.herokuapp.com" className="btn btn-outline-dark" role="button" target="_blank">View Project</a>
           </div>
         </div>
         <div className="card">
           <div className="card-header">Fighting Fit Meetup</div>
-          <img src="" className="card-img-top" alt="..."/>
+          <img src={FightingFitMeetup} className="card-img-top" alt="fighting fit meetup site"/>
           <div className="card-body">
-            <a href="#" className="btn btn-outline-dark" role="button">View Project</a>
+            <a href="#" className="btn btn-outline-dark" role="button" target="_blank">View Project</a>
           </div>
         </div>
         <div className="card">
           <div className="card-header">Pinch of Salt</div>
-          <img src="" class="card-img-top" alt="..."/>
+          <img src={PinchofSalt} className="card-img-top" alt="pinch of salt site"/>
           <div className="card-body">
-            <a href="#" className="btn btn-outline-dark" role="button">View Project</a>
+            <a href="#" className="btn btn-outline-dark" role="button" target="_blank">View Project</a>
           </div>
         </div>
         <div className="card">
           <div className="card-header">A Novel Idea</div>
-          <img src="" className="card-img-top" alt="..."/>
+          <img src="" class="card-img-top" alt="..."/>
           <div className="card-body">
-            <a href="#" className="btn btn-outline-dark" role="button">View Project</a>
+            <a href="https://pinch-of-salt.herokuapp.com/" className="btn btn-outline-dark" role="button" target="_blank">View Project</a>
           </div>
         </div>
       </div>
